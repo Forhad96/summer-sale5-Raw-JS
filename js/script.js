9f44b557f2561ebdf4f9a2d5101cda2e82eca504
Forhad96 <forhadairdrop@gmail.com>
2023-08-21 13:29:59 +0600

    coplete aside bar resposive with mobile device

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 function btnAddToCart(event) {
   const itemName = event.children[2].innerText;
   const itemPrice = event.children[3].children[0].innerText;
@@ -7,11 +6,14 @@ function btnAddToCart(event) {
   const discountTotal = document.getElementById("discount");
   const totalPaymentAmount = document.getElementById("total");
   const purchaseBtn = document.getElementById("purchase-btn");
+  const subTotal = document.getElementById('subtotal');
 
-  const currentTotalPrice = parseFloat(previousTotalPrice.innerText) + parseFloat(itemPrice);
+  const currentTotalPrice =
+    parseFloat(previousTotalPrice.innerText) + parseFloat(itemPrice);
   // set total
   previousTotalPrice.innerText = currentTotalPrice.toFixed(2);
   totalPaymentAmount.innerText = currentTotalPrice.toFixed(2);
+  subTotal.innerText = currentTotalPrice.toFixed(2);
 
   // create element for show item name on card
   const orderList = document.getElementById("order-list");
@@ -21,10 +23,15 @@ function btnAddToCart(event) {
   p.innerHTML = `${count + 1}. ${itemName}`;
   orderList.appendChild(p);
 
+  // badge counting
+  const itemCount = document.querySelectorAll('.item-count');
+  // console.log(itemCount)
+  Array.from(itemCount,e => (e.innerText = count + 1))
+
   // coupon/cart validation
   const withOutDiscountTotal = parseFloat(previousTotalPrice.innerText);
   if (withOutDiscountTotal > 0) {
-    purchaseBtn.removeAttribute('disabled')
+    purchaseBtn.removeAttribute("disabled");
   }
   if (withOutDiscountTotal >= 200) {
     couponBtn.removeAttribute("disabled");
@@ -54,11 +61,34 @@ function btnAddToCart(event) {
 
   // purchase button handler
   document.getElementById("modal-btn").addEventListener("click", function () {
-    purchaseBtn.setAttribute('disabled','true');
-    couponBtn.setAttribute('disabled','true');
-    previousTotalPrice.innerText = '00';
-    discountTotal.innerText = '00';
-    totalPaymentAmount.innerText = '00';
-    p.innerText = '';
+    purchaseBtn.setAttribute("disabled", "true");
+    couponBtn.setAttribute("disabled", "true");
+    previousTotalPrice.innerText = "00";
+    discountTotal.innerText = "00";
+    totalPaymentAmount.innerText = "00";
+    p.innerText = "";
   });
+
+  cartBtn.classList.remove('hidden');
+
 }
+
+// cart button for mobile devices
+const cartBtn = document.getElementById("cart-btn")
+cartBtn.addEventListener("click", function () {
+  // console.log('btn clicked')
+  const aside = document.getElementById("aside-container");
+  // console.log(aside.classList);
+  // aside.classList.remove('hidden')
+  aside.classList.toggle('hidden')
+});
+
+// scroll
+// function scrollBtn(){
+//   cartBtn.classList.remove('hidden');
+
+//   setTimeout(() =>{
+//   cartBtn.classList.add('hidden');
+
+//   },10000)
+// }
